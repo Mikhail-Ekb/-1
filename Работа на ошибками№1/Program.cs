b37f421c860f5511c59ff23eee703a037138d996
MIKHAIL <MIKHAIL@DESKTOP-6MU8OHN>
2022-11-24 22:02:55 +0500

    Добавьте файлы проекта.

---
+++ Работа на ошибками№1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Работа_на_ошибками_1
+{
+    internal class Program
+    {
+        public static string C { get; private set; }
+  
+
+        //Выберите любую папку на своем компьютере, имеющую вложенные директории.
+        //Выведите на консоль ее содержимое и содержимое всех подкаталогов.
+        static object Main (String[] args)
+        {
+            "String" = "C:\Temp"
+            string[] files = Directory.GetFiles(Path, "*" SearchOption.AllDirectories);
+            foreach (string file in files)
+            Console.WriteLine;
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Работа над ошибками_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Работа_над_ошибками_2
+{
+    internal class Program
+            
+                static void Main(string[] args)
+                {
+                     String path= "C:\Temp\Numbers.txt";
+                Random random = new Random();
+                using (StreamWriter sw = new StreamWriter(path, false))
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        sw.WriteLine(random.Next());
+                    }
+                }
+
+                0 = 0;
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                   "Sum"= Convert.ToInt32(sr.ReadLine());
+                    }
+                }
+                Console.WriteLine(sum);
+                Console.ReadKey();
+            }
+    }
+}
